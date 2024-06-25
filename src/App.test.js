90530163e203bf4914c0a33ff39b770ec9dccb82
Rabia-Rajput <rajput.rabia@outlook.com>
2024-06-25 13:29:15 +0200

    update test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,18 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText('hi there!');
   expect(linkElement).toBeInTheDocument();
 });
+
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// it('renders learn react link', () => {
+//   const div= document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   expect(div.innerHTML).toContain(('hi there!'))
+// ReactDOM.unmountComponentAtNode(div);
+// });
+
