d820934c1ccab0af7fc3131e99b043a8ad0dd132
Rabia-Rajput <rajput.rabia@outlook.com>
2024-06-26 17:35:58 +0200

    update test file

--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -1,20 +1,16 @@
-// import { render, screen } from '@testing-library/react';
-// import App from '../App';
-
-// test('renders CommentBox and CommentList components', () => {
-//   render(<App />);
-//   const commentBox  = screen.getByText(/Comment Box/i);
-//   expect(commentBox ).toBeInTheDocument();
-// });
-
  import { render, screen } from '@testing-library/react';
  import App from '../App';
- import React from 'react';
- import ReactDOM from 'react-dom';
- it('shows a comment box', () => {
-   const div= document.createElement('div');
-   ReactDOM.render(<App />, div);
-  expect(div.innerHTML).toContain('Comment Box')
- ReactDOM.unmountComponentAtNode(div);
- });
+describe('renders CommentBox and CommentList components',()=>{
+    it('renders CommentBox ', () => {
+        render(<App />);
+       const commentBox  = screen.getByText(/Comment Box/i);
+     
+        expect(commentBox ).toBeInTheDocument();
+      });
+      it('renders CommentList component', () => {
+        render(<App />);
+     const commentList =screen.getByText(/Comment List/i);
+     expect(commentList ).toBeInTheDocument();
+    });
+})
 
