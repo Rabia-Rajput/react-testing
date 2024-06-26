4c6d7f794b151d587b7de8142bd16cd6dc4f8532
Rabia-Rajput <rajput.rabia@outlook.com>
2024-06-26 17:35:31 +0200

    update CommentBox

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,7 +1,30 @@
-import React from 'react'
 
-export default  () => {
+import React, { useState } from 'react';
+
+const CommentBox = ({ addComment }) => {
+  const [comment, setComment] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (comment.trim() !== '') {
+      addComment(comment);
+      setComment(''); 
+    }
+  };
+
+  const handleChange = (event) => {
+    setComment(event.target.value);
+  };
+
   return (
-    <div>Comment Box</div>
-  )
-}
+    <div>
+      <form onSubmit={handleSubmit}>
+        <h4>Add A Comment</h4>
+        <textarea value={comment} onChange={handleChange}></textarea>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default CommentBox;
